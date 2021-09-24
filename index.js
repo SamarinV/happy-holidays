766b6d0415fd2945610f1b0ed2658bdac492d559
Vladimir Samarin <v.a.samarin@yandex.ru>
2021-09-24 17:41:46 +0300

    добавил кнопку у видео

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const video = document.querySelector('.video__file'); // присвоили пе
 const videoText = document.querySelector('.video__text'); //присвоили переменной videoText класс нашего текста
 video.addEventListener('play', () => {videoText.style.display = ('none')}); //слушаем событие play и меняем стиль
                                                                             // на display none
+
+var videoBlock = document.getElementById('video-block'),
+vid = document.getElementById('vid');
+videoBlock.addEventListener('click', function() {
+if (vid.paused) {
+  vid.play();
+  videoBlock.classList.remove('paused');
+} else {
+  vid.pause();
+  videoBlock.classList.add('paused');
+};
+})
